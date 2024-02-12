b6a00d8273731ba699fe1e7583dc7797053de9a6
shawen17 <shawen022@yahoo.com>
2024-02-11 20:17:49 -0600

    authentication model created

--- lendsqr/models.py
+++ lendsqr/models.py
@@ -1,3 +1,50 @@
+from __future__ import unicode_literals
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from datetime import datetime,date
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
+from .managers import UserManager
+from django.utils import timezone
+from django.contrib.auth.models import Group, Permission
 
-# Create your models here.
+
+
+class User(AbstractUser):
+    username= None
+    email=models.EmailField(_('email address'),unique=True)
+    state=models.CharField(max_length=50,default='Lagos')
+
+
+    groups = models.ManyToManyField(
+        Group,
+        verbose_name=_('groups'),
+        blank=True,
+        related_name='%(class)s_groups'  # Custom related_name for groups
+    )
+    user_permissions = models.ManyToManyField(
+        Permission,
+        verbose_name=_('user permissions'),
+        blank=True,
+        related_name='%(class)s_user_permissions'  # Custom related_name for user_permissions
+    )
+    
+
+    class Meta:
+        verbose_name=_('user')
+        verbose_name_plural=_('users')
+
+    objects=UserManager()
+
+    USERNAME_FIELD='email'
+    REQUIRED_FIELDS=['first_name','last_name','state','password']
+
+    def __str__(self):
+        return self.email
+
+
+    def get_full_name(self):
+        return f'{self.first_name} {self.last_name}'
+
+    def get_short_name(self):
+        return self.first_name
