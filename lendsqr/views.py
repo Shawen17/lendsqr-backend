0966b3f2024e2063fba478d63af226666ec0738e
shawen17 <shawen022@yahoo.com>
2024-02-21 18:39:38 -0500

    database changed to docker mongo container

--- lendsqr/views.py
+++ lendsqr/views.py
@@ -19,9 +19,10 @@ from rest_framework.permissions import IsAuthenticated
 db_user = config("DB_USER")
 db_password=config("PASSWORD")
 db_cluster = config("CLUSTERNAME")
-client = pymongo.MongoClient(
-    f"mongodb+srv://{db_user}:{db_password}@{db_cluster}.jzsljb4.mongodb.net/?retryWrites=true&w=majority")
+# client = pymongo.MongoClient(
+#     f"mongodb+srv://{db_user}:{db_password}@{db_cluster}.jzsljb4.mongodb.net/?retryWrites=true&w=majority")
 
+client = pymongo.MongoClient(f"mongodb://{db_user}:{db_password}@{db_cluster}/")
 db = client['user_details']
 
 
