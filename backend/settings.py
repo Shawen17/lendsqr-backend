78e6e87763d0b860c74239535c03dd3d586acf16
shawen17 <shawen022@yahoo.com>
2024-02-26 13:46:31 -0500

    debug

--- backend/settings.py
+++ backend/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["localhost","127.0.0.1",'oluwaseun-johnson-lendsqr-fe-test.netlify.app']
 
