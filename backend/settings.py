66f078d140e84c6cd5a666cc1552c8760b5a5d0c
shawen17 <shawen022@yahoo.com>
2024-03-01 23:55:44 -0500

    aws media upload

--- backend/settings.py
+++ backend/settings.py
@@ -132,8 +132,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 
 
-# Internationalization
-# https://docs.djangoproject.com/en/4.1/topics/i18n/
+
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         # 'rest_framework.permissions.IsAuthenticatedOrReadOnly',
